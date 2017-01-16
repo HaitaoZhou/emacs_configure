db9d62b9282785df25c257a6e0d12e87c7bb84da
Haitao Zhou <zhouht@live.com>
2017-01-16 20:08:48 +0800

    add tab and backtab for indent

--- lisp/init_better_default.el
+++ lisp/init_better_default.el
@@ -21,17 +21,17 @@
 (when (version< emacs-version "25")
   (defadvice show-paren-function (around fix-show-paren-function activate)
     (cond ((looking-at-p "\\s(") ad-do-it)
-	  (t (save-excursion
-	       (ignore-errors (backward-up-list))
-	       ad-do-it)))
+          (t (save-excursion
+               (ignore-errors (backward-up-list))
+               ad-do-it)))
     ))
 (when (version<= "25" emacs-version)
   (define-advice show-paren-function (:around (fn) fix-show-paren-function)
     "Highlight enclosing parens."
     (cond ((looking-at-p "\\s(") (funcall fn))
-	  (t (save-excursion
-	       (ignore-errors (backward-up-list))
-	       (funcall fn))))))
+          (t (save-excursion
+               (ignore-errors (backward-up-list))
+               (funcall fn))))))
 ;; 开启括号高亮配对模式
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
@@ -47,24 +47,24 @@
   (interactive)
   (save-excursion
     (if (region-active-p)
-	(progn
-	  (indent-region (region-beginning) (region-end))
-	  (message "Indent selected region."))
+        (progn
+          (indent-region (region-beginning) (region-end))
+          (message "Indent selected region."))
       (progn
-	(indent-buffer)
-	(message "Indent buffer.")))))
+        (indent-buffer)
+        (message "Indent buffer.")))))
 
 ;; 设定hippie代码补全
 (setq hippie-expand-try-function-list '(try-expand-debbrev
-					try-expand-debbrev-all-buffers
-					try-expand-debbrev-from-kill
-					try-complete-file-name-partially
-					try-complete-file-name
-					try-expand-all-abbrevs
-					try-expand-list
-					try-expand-line
-					try-complete-lisp-symbol-partially
-					try-complete-lisp-symbol))
+                                        try-expand-debbrev-all-buffers
+                                        try-expand-debbrev-from-kill
+                                        try-complete-file-name-partially
+                                        try-complete-file-name
+                                        try-expand-all-abbrevs
+                                        try-expand-list
+                                        try-expand-line
+                                        try-complete-lisp-symbol-partially
+                                        try-complete-lisp-symbol))
 
 ;; 文件目录操作取消递归确认
 (setq dired-recursive-deletes 'always)
@@ -92,17 +92,69 @@
   "Call `occur' with a sane default."
   (interactive)
   (push (if (region-active-p)
-	    (buffer-substring-no-properties
-	     (region-beginning)
-	     (region-end))
-	  (let ((sym (thing-at-point 'symbol)))
-	    (when (stringp sym)
-	      (regexp-quote sym))))
-	regexp-history)
+            (buffer-substring-no-properties
+             (region-beginning)
+             (region-end))
+          (let ((sym (thing-at-point 'symbol)))
+            (when (stringp sym)
+              (regexp-quote sym))))
+        regexp-history)
   (call-interactively 'occur))
 
 ;; imuenu list config
 (setq imenu-list-focus-after-activation t)
 (setq imenu-list-auto-resize t)
+
+;; 鼠标滚轮，默认的滚动太快，这里改为3行
+(defun up-slightly () (interactive) (scroll-up 3))
+(defun down-slightly () (interactive) (scroll-down 3))
+(global-set-key [mouse-4] 'down-slightly)
+(global-set-key [mouse-5] 'up-slightly)
+
+;; my fix for tab indent
+(defun haitao/indent-region(numSpaces)
+  (progn
+    ;; default to start and end of current line
+    (setq regionStart (line-beginning-position))
+    (setq regionEnd (line-end-position))
+
+    ;; if there's a selection, use that instead of the current line
+    (when (use-region-p)
+      (setq regionStart (region-beginning))
+      (setq regionEnd (region-end))
+      )
+
+    (save-excursion                          ; restore the position afterwards
+      (goto-char regionStart)                ; go to the start of region
+      (setq start (line-beginning-position)) ; save the start of the line
+      (goto-char regionEnd)                  ; go to the end of region
+      (setq end (line-end-position))         ; save the end of the line
+
+      (indent-rigidly start end numSpaces) ; indent between start and end
+      (setq deactivate-mark nil)           ; restore the selected region
+      )
+    )
+  )
+
+(defun haitao/tab-region(N)
+  (interactive "p")
+  (if (use-region-p)
+      (haitao/indent-region 4)               ; region was selected, call indent-region
+    (insert "    ")                   ; else insert four spaces as expected
+    ))
+
+(defun haitao/untab-region(N)
+  (interactive "p")
+  (haitao/indent-region -4))
+
+(defun haitao/hack-tab-key ()
+  (interactive)
+  (local-set-key (kbd "<tab>") 'haitao/tab-region)
+  (local-set-key (kbd "<backtab>") 'haitao/untab-region)
+  )
+
+(add-hook 'prog-mode-hook 'haitao/hack-tab-key)
+(add-hook 'text-mode-hook 'haitao/hack-tab-key)
+
 
 (provide 'init_better_default)
