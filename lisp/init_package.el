a9c7c148b6efd03cb1af90b69fffef7513fe5a26
Haitao Zhou <zhouht@live.com>
2017-01-22 21:30:44 +0800

    config python mode

--- lisp/init_package.el
+++ lisp/init_package.el
@@ -10,20 +10,24 @@
 
 ;; Add Packages
 (defvar haitao/packages '(
-			  company
-			  hungry-delete
-			  swiper
-			  counsel
-			  smex
-			  smartparens
-			  popwin
-			  iedit
-			  which-key
-			  yasnippet
-			  monokai-theme
-			  youdao-dictionary
-			  imenu-list
-			  ) "Default packages")
+                          company
+                          hungry-delete
+                          swiper
+                          counsel
+                          smex
+                          smartparens
+                          popwin
+                          iedit
+                          which-key
+                          yasnippet
+                          monokai-theme
+                          youdao-dictionary
+                          imenu-list
+                          elpy
+                          flycheck
+                          py-autopep8
+                          company-anaconda
+                          ) "Default packages")
 
 (setq package-selected-packages haitao/packages)
 
@@ -54,7 +58,11 @@
 (setq ivy-use-virtual-buffers t)
 
 ;; 开启全局 Company 补全
-(global-company-mode t)
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(setq company-transformers '(company-sort-by-occurrence))
+(eval-after-load "company"
+  '(add-to-list 'company-backends '(company-anaconda :with company-yasnippet)))
 
 ;; require dired-x for c-x c-j打开文件所在目录
 (require 'dired-x)
@@ -67,15 +75,27 @@
 (which-key-mode)
 
 ;; yasnippet
-(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets"                 ;; personal snippets
-	"~/.emacs.d/elpa/yasnippet-20161221.1953/snippets"         ;; the default collection
-	))
+        "~/.emacs.d/elpa/yasnippet-20161221.1953/snippets" ;; the default collection
+        ))
 (yas-global-mode 1)
+
 
 ;; cua-mode
 (cua-mode 1)
+
+;; python-mode config
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'elpy-mode)
+(when (require 'flycheck nil t)
+  (require 'elpy)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
 
 (provide 'init_package)
